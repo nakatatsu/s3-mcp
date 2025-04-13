0e8c4b0e82e609ab927a0d64b7432eaab6d31c28
ashgw <ashrefgw@proton.me>
2025-04-13 22:39:50 +0100

    fix env

--- env.py
+++ env.py
@@ -5,11 +5,17 @@ class EnvConfig:
     def __init__(self):
         self.env = Env()
         self.env.read_env()
+    @property
+    def AWS_ACCESS_KEY_ID(self):
+        return self.env("AWS_ACCESS_KEY_ID")
 
     @property
-    def OPEN_AI_KEY(self):
-        return self.env("OPENAI_API_KEY")
+    def AWS_SECRET_ACCESS_KEY(self):
+        return self.env("AWS_SECRET_ACCESS_KEY")
 
+    @property
+    def AWS_REGION(self):
+        return self.env("AWS_REGION", "us-west-1")
 
 env = EnvConfig()
 
